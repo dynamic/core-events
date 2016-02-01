19baf988609f139401fe19855375f7e4ad109fd8
muskie9 <nhorstmeier@gmail.com>
2016-02-01 08:54:11 -0600

    BUGFIX Feed Events don't take into account daylight savings time
    fixes #3
    
    see dynamic/core#85

--- code/pages/EventHolder.php
+++ code/pages/EventHolder.php
@@ -1,133 +1,136 @@
 <?php
 
-    class EventHolder extends HolderPage
+class EventHolder extends HolderPage
+{
+    public static $item_class = 'EventPage';
+    private static $allowed_children = array('EventPage');
+    private static $singular_name = 'Event Holder';
+    private static $plural_name = 'Events Holder';
+    private static $description = 'Page holding events, displays child pages that are events';
+
+    private static $timezone = 'America/Chicago';
+
+    private static $db = array(
+        'ICSFeed' => 'Varchar(255)',
+        'EventsPerPage' => 'Int',
+        //0 == All
+        'RangeToShow' => 'Enum("Month,Year,All Upcoming","Month")',
+        //TODO add day option, bug in getFeedEvents date logic
+    );
+
+    public function getCMSFields()
     {
-        public static $item_class = 'EventPage';
-        private static $allowed_children = array('EventPage');
-        private static $singular_name = 'Event Holder';
-        private static $plural_name = 'Events Holder';
-        private static $description = 'Page holding events, displays child pages that are events';
+        $fields = parent::getCMSFields();
 
-        private static $timezone = 'America/Chicago';
-
-        private static $db = array(
-            'ICSFeed' => 'Varchar(255)',
-            'EventsPerPage' => 'Int',//0 == All
-            'RangeToShow' => 'Enum("Month,Year,All Upcoming","Month")',//TODO add day option, bug in getFeedEvents date logic
+        $fields->addFieldToTab('Root.Main', TextField::create('ICSFeed')->setTitle('ICS Feed URL'), 'Content');
+        $fields->addFieldToTab(
+            'Root.Main',
+            DropdownField::create(
+                'RangeToShow',
+                'Range to show',
+                singleton('EventHolder')->dbObject('RangeToShow')->enumValues()
+            ),
+            'Content'
+        );
+        $fields->addFieldToTab(
+            'Root.Main',
+            NumericField::create(
+                'EventsPerPage'
+            )->setTitle('Events to show per page (0 shows all based on the "Rage to show")'),
+            'Content'
         );
 
-        public function getCMSFields()
-        {
-            $fields = parent::getCMSFields();
+        return $fields;
+    }
 
-            $fields->addFieldToTab('Root.Main', TextField::create('ICSFeed')->setTitle('ICS Feed URL'), 'Content');
-            $fields->addFieldToTab(
-                'Root.Main',
-                DropdownField::create(
-                    'RangeToShow',
-                    'Range to show',
-                    singleton('EventHolder')->dbObject('RangeToShow')->enumValues()
-                ),
-                'Content'
-            );
-            $fields->addFieldToTab(
-                'Root.Main',
-                NumericField::create(
-                    'EventsPerPage'
-                )->setTitle('Events to show per page (0 shows all based on the "Rage to show")'),
-                'Content'
-            );
+    public function getFeedEvents($start_date = null, $end_date = null)
+    {
+        $start = sfDate::getInstance(strtotime('now'));
+        $end_date = $this->buildEndDate($start);
 
-            return $fields;
+        // single day views don't pass end dates
+        if ($end_date) {
+            $end = sfDate::getInstance($end_date);
+        } else {
+            $end = false;
         }
 
-        public function getFeedEvents($start_date = null, $end_date = null)
-        {
-            $start = sfDate::getInstance(strtotime('now'));
-            $end_date = $this->buildEndDate($start);
+        $feedevents = new ArrayList();
+        $feedreader = new ICSReader($this->ICSFeed);
+        $events = $feedreader->getEvents();
+        foreach ($events as $event) {
+            // translate iCal schema into CalendarAnnouncement schema (datetime + title/content)
+            $feedevent = new EventPage();
+            $feedevent->Title = $event['SUMMARY'];
+            if (isset($event['DESCRIPTION'])) {
+                $feedevent->Content = $event['DESCRIPTION'];
+            }
 
-            // single day views don't pass end dates
-            if ($end_date) {
-                $end = sfDate::getInstance($end_date);
+            $startdatetime = $this->iCalDateToDateTime($event['DTSTART']);
+            $enddatetime = $this->iCalDateToDateTime($event['DTEND']);
+
+            if (($end !== false) && (($startdatetime->get() < $start->get() && $enddatetime->get() < $start->get())
+                    || $startdatetime->get() > $end->get() && $enddatetime->get() > $end->get())
+            ) {
+                // do nothing; dates outside range
             } else {
-                $end = false;
-            }
+                if ($startdatetime->get() > $start->get()) {
+                    $feedevent->Date = $startdatetime->format('Y-m-d');
+                    $feedevent->Time = $startdatetime->format('H:i:s');
 
-            $feedevents = new ArrayList();
-            $feedreader = new ICSReader($this->ICSFeed);
-            $events = $feedreader->getEvents();
-            foreach ($events as $event) {
-                // translate iCal schema into CalendarAnnouncement schema (datetime + title/content)
-                $feedevent = new EventPage();
-                $feedevent->Title = $event['SUMMARY'];
-                if (isset($event['DESCRIPTION'])) {
-                    $feedevent->Content = $event['DESCRIPTION'];
-                }
+                    $feedevent->EndDate = $enddatetime->format('Y-m-d');
+                    $feedevent->EndTime = $enddatetime->format('H:i:s');
 
-                $startdatetime = $this->iCalDateToDateTime($event['DTSTART']);
-                $enddatetime = $this->iCalDateToDateTime($event['DTEND']);
-
-                if (($end !== false) && (($startdatetime->get() < $start->get() && $enddatetime->get() < $start->get())
-                    || $startdatetime->get() > $end->get() && $enddatetime->get() > $end->get())) {
-                    // do nothing; dates outside range
-                } else {
-                    if ($startdatetime->get() > $start->get()) {
-                        $feedevent->Date = $startdatetime->format('Y-m-d');
-                        $feedevent->Time = $startdatetime->format('H:i:s');
-
-                        $feedevent->EndDate = $enddatetime->format('Y-m-d');
-                        $feedevent->EndTime = $enddatetime->format('H:i:s');
-
-                        $feedevents->push($feedevent);
-                    }
+                    $feedevents->push($feedevent);
                 }
             }
-
-            return $feedevents;
         }
 
-        public function iCalDateToDateTime($date)
-        {
-            date_default_timezone_set($this->stat('timezone'));
-            $date = str_replace('T', '', $date);//remove T
-            $date = str_replace('Z', '', $date);//remove Z
-            $date = strtotime($date);
-            $date = $date + date('Z');
+        return $feedevents;
+    }
 
-            return sfDate::getInstance($date);
+    public function iCalDateToDateTime($date)
+    {
+        date_default_timezone_set($this->stat('timezone'));
+        $date = str_replace('T', '', $date);//remove T
+        $date = str_replace('Z', '', $date);//remove Z
+        $date = strtotime($date);
+        $date = (!date('I')) ? $date : strtotime('- 1 hour', $date);
+        $date = $date + date('Z');
+        return sfDate::getInstance($date);
+    }
+
+    public function buildEndDate($start = null)
+    {
+        if ($start === null) {
+            $start = sfDate::getInstance(strtotime('now'));
         }
 
-        public function buildEndDate($start = null)
-        {
-            if ($start === null) {
-                $start = sfDate::getInstance(strtotime('now'));
-            }
-
-            switch ($this->RangeToShow) {
-                case 'Day':
-                    $end_date = $start;
-                    break;
-                case 'Year':
-                    $end_date = date('Y-m-d', strtotime(date('Y-m-d', time()).' + 365 day'));
-                    break;
-                case 'All Upcoming':
-                    $end_date = false;
-                    break;
-                default:
-                    $end_date = date('Y-m-d', strtotime(date('Y-m-d', time()).' + 1 month'));
-                    break;
-            }
-
-            return $end_date;
+        switch ($this->RangeToShow) {
+            case 'Day':
+                $end_date = $start;
+                break;
+            case 'Year':
+                $end_date = date('Y-m-d', strtotime(date('Y-m-d', time()) . ' + 365 day'));
+                break;
+            case 'All Upcoming':
+                $end_date = false;
+                break;
+            default:
+                $end_date = date('Y-m-d', strtotime(date('Y-m-d', time()) . ' + 1 month'));
+                break;
         }
 
-        public static function getUpcomingEvents($filter = array(), $limit = 10)
-        {
-            $filter['Date:GreaterThanOrEqual'] = date('Y-m-d', strtotime('now'));
-            $events = ($limit == 0) ?
-                EventPage::get()
-                    ->filter($filter)
-                    ->sort('Date', 'ASC')
+        return $end_date;
+    }
+
+    public static function getUpcomingEvents($filter = array(), $limit = 10)
+    {
+        $filter['Date:GreaterThanOrEqual'] = date('Y-m-d', strtotime('now'));
+        $events = ($limit == 0) ?
+            EventPage::get()
+                ->filter($filter)
+                ->sort('Date', 'ASC')
 
             :
             EventPage::get()
@@ -135,121 +138,122 @@
                 ->limit($limit)
                 ->sort('Date', 'ASC');
 
-            return $events;
-        }
-
-        public function getEvents($filter = null, $limit = 10)
-        {
-            $eventList = ArrayList::create();
-            $events = self::getUpcomingEvents($filter, $limit);
-            $eventList->merge($events);
-            if ($this->ICSFeed) {
-                $icsEvents = $this->getFeedEvents();
-                $eventList->merge($icsEvents);
-            }
-
-            return $eventList;
-        }
-
-        public function getItemsShort()
-        {
-            return EventPage::get()
-                ->limit(3)
-                ->filter(array(
-                    'Date:LessThan:Not' => date('Y-m-d', strtotime('now')),
-                    'ParentID' => $this->ID, ))
-                ->sort('Date', 'ASC');
-        }
-
-        /**
-         * @param Member $member
-         *
-         * @return bool
-         */
-        public function canView($member = null)
-        {
-            return parent::canView($member = null);
-        }
-
-        public function canEdit($member = null)
-        {
-            return Permission::check('EventHolder_CRUD');
-        }
-
-        public function canDelete($member = null)
-        {
-            return Permission::check('EventHolder_CRUD');
-        }
-
-        public function canCreate($member = null)
-        {
-            return Permission::check('EventHolder_CRUD');
-        }
-
-        public function providePermissions()
-        {
-            return array(
-                //'Location_VIEW' => 'Read a Location',
-                'EventHolder_CRUD' => 'Create, Update and Delete an Event Holder Page',
-            );
-        }
+        return $events;
     }
 
-    class EventHolder_Controller extends HolderPage_Controller
+    public function getEvents($filter = null, $limit = 10)
     {
-        public function init()
-        {
-            parent::init();
+        $eventList = ArrayList::create();
+        $events = self::getUpcomingEvents($filter, $limit);
+        $eventList->merge($events);
+        if ($this->ICSFeed) {
+            $icsEvents = $this->getFeedEvents();
+            $eventList->merge($icsEvents);
         }
 
-        private static $allowed_actions = array(
-            'tag',
+        return $eventList;
+    }
+
+    public function getItemsShort()
+    {
+        return EventPage::get()
+            ->limit(3)
+            ->filter(array(
+                'Date:LessThan:Not' => date('Y-m-d', strtotime('now')),
+                'ParentID' => $this->ID,
+            ))
+            ->sort('Date', 'ASC');
+    }
+
+    /**
+     * @param Member $member
+     *
+     * @return bool
+     */
+    public function canView($member = null)
+    {
+        return parent::canView($member = null);
+    }
+
+    public function canEdit($member = null)
+    {
+        return Permission::check('EventHolder_CRUD');
+    }
+
+    public function canDelete($member = null)
+    {
+        return Permission::check('EventHolder_CRUD');
+    }
+
+    public function canCreate($member = null)
+    {
+        return Permission::check('EventHolder_CRUD');
+    }
+
+    public function providePermissions()
+    {
+        return array(
+            //'Location_VIEW' => 'Read a Location',
+            'EventHolder_CRUD' => 'Create, Update and Delete an Event Holder Page',
         );
+    }
+}
 
-        public function Items($filter = array(), $pageSize = 10)
-        {
-            $filter['ParentID'] = $this->Data()->ID;
-            $class = $this->Data()->stat('item_class');
+class EventHolder_Controller extends HolderPage_Controller
+{
+    public function init()
+    {
+        parent::init();
+    }
 
-            $items = $this->getUpcomingEvents($filter);
+    private static $allowed_actions = array(
+        'tag',
+    );
 
-            $list = PaginatedList::create($items, $this->request);
-            $list->setPageLength($pageSize);
+    public function Items($filter = array(), $pageSize = 10)
+    {
+        $filter['ParentID'] = $this->Data()->ID;
+        $class = $this->Data()->stat('item_class');
 
-            return $list;
-        }
+        $items = $this->getUpcomingEvents($filter);
 
-        public function tag()
-        {
-            $request = $this->request;
-            $params = $request->allParams();
+        $list = PaginatedList::create($items, $this->request);
+        $list->setPageLength($pageSize);
 
-            if ($tag = Convert::raw2sql(urldecode($params['ID']))) {
-                $filter = array('Tags.Title' => $tag);
+        return $list;
+    }
 
-                return $this->customise(array(
-                    'Message' => 'showing entries tagged "'.$tag.'"',
-                    'Items' => $this->Items($filter),
-                ));
-            }
+    public function tag()
+    {
+        $request = $this->request;
+        $params = $request->allParams();
 
-            return $this->Items();
-        }
+        if ($tag = Convert::raw2sql(urldecode($params['ID']))) {
+            $filter = array('Tags.Title' => $tag);
 
-        public function getUpcomingEvents($filter = array())
-        {
-            $pageSize = ($this->data()->EventsPerPage == 0) ? 10 : $this->data()->EventsPerPage;
-
-            $filter['EndDate:GreaterThanOrEqual'] = date('Y-m-d', strtotime('now'));
-            if ($this->data()->RangeToShow != 'All Upcoming') {
-                $end_date = $this->data()->buildEndDate();
-                $filter['Date:LessThanOrEqual'] = $end_date;
-            }
-            $items = $this->data()->getEvents($filter, 0);
-
-            return $items->sort(array(
-                'Date' => 'ASC',
-                'Time' => 'ASC',
+            return $this->customise(array(
+                'Message' => 'showing entries tagged "' . $tag . '"',
+                'Items' => $this->Items($filter),
             ));
         }
+
+        return $this->Items();
     }
+
+    public function getUpcomingEvents($filter = array())
+    {
+        $pageSize = ($this->data()->EventsPerPage == 0) ? 10 : $this->data()->EventsPerPage;
+
+        $filter['EndDate:GreaterThanOrEqual'] = date('Y-m-d', strtotime('now'));
+        if ($this->data()->RangeToShow != 'All Upcoming') {
+            $end_date = $this->data()->buildEndDate();
+            $filter['Date:LessThanOrEqual'] = $end_date;
+        }
+        $items = $this->data()->getEvents($filter, 0);
+
+        return $items->sort(array(
+            'Date' => 'ASC',
+            'Time' => 'ASC',
+        ));
+    }
+}
